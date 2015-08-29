cca1d9e882a91229e68b2061a5219afb73b89d65
Adrienne Dreyfus <adrienne.dreyfus@tufts.edu>
2015-08-28 20:47:59 -0700

    Removed Angular replaced with Ember

--- server.js
+++ server.js
@@ -18,8 +18,14 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); //So our Ember app can communicate
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
@@ -28,3 +34,4 @@ require('./app/routes')(app); // pass our application into our routes
 app.listen(port);	
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
+
