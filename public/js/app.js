aae6e619cfd7e763815f03f2a1efce4d5be150f7
cutofmyjib <thedianalozano@gmail.com>
2015-08-28 22:55:10 -0700

    add satellizer authprovider

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+
+angular.module('MyApp', ['satellizer'])
+  .config(function($authProvider) {
+
+    $authProvider.linkedin({
+      clientId: 'LinkedIn Client ID'
+    });
+
+  });)
