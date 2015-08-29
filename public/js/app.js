87a5a40c85ce15e80dcbc1a5705b503af16d0a52
Adrienne Dreyfus <adrienne.dreyfus@tufts.edu>
2015-08-29 12:22:37 -0700

    Updated to 57

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 angular.module('sampleApp', ['satellizer', 'ngRoute', 'appRoutes', 'LoginCtrl', 'UserCtrl', 'UserService', 'CompanyCtrl','CompanyService', 'TrainingCtrl', 'ProfileCtrl', 'AboutUsCtrl']).config(function($authProvider){
     $authProvider.linkedin({
       clientId: '75o7oq8qxk1g5s',
-      redirectUri: 'http://menther-50755.onmodulus.net/nerds',
+      redirectUri: 'http://menther-50757.onmodulus.net/nerds',
       scope: ['r_emailaddress', 'r_basicprofile'],
     });
 });
