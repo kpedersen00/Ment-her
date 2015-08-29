113bacbc9c9e535bb4e6a37c799715dc3b88e49d
cutofmyjib <thedianalozano@gmail.com>
2015-08-29 00:15:32 -0700

    cleaned up app js

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,9 @@
-// angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('sampleApp', ['satellizer','ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
 
-angular.module('MyApp', ['satellizer'])
-  .config(function($authProvider) {
+.config(function($authProvider) {
 
     $authProvider.linkedin({
       clientId: '75dtrr7fhjol46'
     });
 
-  });)
+  });
